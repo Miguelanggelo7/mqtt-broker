d6523d8c338f5a81c787a6eedb0adf4ba1fb3ffe
gustavo-dgs <gustavo.dgs19@gmail.com>
2023-01-19 17:24:46 -0400

    Last change

--- src/services/MqttWeb/index.js
+++ src/services/MqttWeb/index.js
@@ -11,14 +11,37 @@ ws.createServer({ server: httpServer }, aedes.handle);
 
 class MqttWeb {
   static publishService;
+  static aedes = aedes;
 
   static mainPublish(topic, payload, callback) {
-    aedes.publish(MqttWeb.setPacket(topic, payload), callback);
+    this.aedes.publish(MqttWeb.setPacket(topic, payload), callback);
   }
 
   static initService() {
+    this.aedes.on("clientReady", async (client) => {
+      console.log(
+        `[CLIENT_CONNECTED] Client ${client ? client.id : client} on WebBroker`
+      );
+    });
+
+    this.aedes.on("client", async (client) => {
+      console.log(
+        `[CLIENT_CONNECTED] Client ${client ? client.id : client} on WebBroker`
+      );
+    });
+
+    // emitted when a client disconnects from the broker
+    aedes.on("clientDisconnect", async (client) => {
+      console.log(
+        `[CLIENT_DISCONNECTED] Client ${client ? client.id : client}`
+      );
+    });
+
     // eslint-disable-next-line no-unused-vars
-    aedes.on("publish", async (packet, client) => {
+    this.aedes.on("publish", async (packet, client) => {
+      console.log(
+        `[WEB_MESSAGE_PUBLISHED] Client ${client?.id} has published message ${packet.payload} on ${packet.topic}`
+      );
       await MqttWeb.publishService(packet.topic, packet.payload);
     });
   }
